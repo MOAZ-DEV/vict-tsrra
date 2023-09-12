f7a242a4353e2fda83d8be0ec4025df7f7ee1610
Moaz Alla Elden <91629974+MOAZ-DEV@users.noreply.github.com>
2023-09-12 10:50:35 +0200

    .

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './assets/styling/App.scss'
+
+export const App = (_props:any) => {
+
+  return <>
+    Hi
+  </>
+}
---
+++ src/components/component.tsx
@@ -0,0 +1,5 @@
+
+export const component = (_props: any) => {
+    return <>
+    </>
+}
